df89972e1e7950968e05b14554d9aa6fa8588cd5
Jaeseong Jeong <js104427@gmail.com>
2023-05-19 10:45:57 +0800

    added renderComponent() instead of each rendering

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,7 +2,7 @@ import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from './UserList';
 
-test('render the correct number of rows', () => {
+function renderComponent() {
     // Render the component
     const users = [
         { name: 'jane', email: 'jane@jane.com' },
@@ -10,6 +10,13 @@ test('render the correct number of rows', () => {
     ];
     render(<UserList users={users} />);
 
+    return {
+        users,
+    };
+}
+
+test('render the correct number of rows', () => {
+    const { users } = renderComponent();
     // Find all the rows in the table
     const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
@@ -18,13 +25,7 @@ test('render the correct number of rows', () => {
 });
 
 test('render the email and name of each user', () => {
-    // Render the component
-    const users = [
-        { name: 'jane', email: 'jane@jane.com' },
-        { name: 'john', email: 'john@john.com' },
-    ];
-    render(<UserList users={users} />);
-
+    const { users } = renderComponent();
     for (let user of users) {
         const name = screen.getByRole('cell', { name: user.name });
         const email = screen.getByRole('cell', { name: user.email });
